b5d00bac5346268dcd7e8afdad289f097d261757
Abir Pal <30786216+abir4u@users.noreply.github.com>
2018-03-21 11:05:50 +1300

    Added a few styles for the Secure page

--- app/components/secured/SecuredStyles.js
+++ app/components/secured/SecuredStyles.js
@@ -6,5 +6,29 @@ export default StyleSheet.create({
     position: "absolute",
     width: "100%",
     height: "100%"
+  },
+  securedHeaderPanel: {
+    padding: 15
+  },
+  securedHeaderText: {
+    fontSize: 27,
+    color: "#FFFFFF",
+    textAlign: "center"
+  },
+  logoutButton: {
+    backgroundColor: "#0033CCAA",
+    padding: 15,
+    borderRadius: 3
+  },
+  logoutButtonText: {
+    color: "#FFFFFF",
+    textAlign: "center",
+    fontWeight: "bold"
+  },
+  securedScroll: {
+    padding: 20
+  },
+  separatorView: {
+    margin: 20
   }
 });
